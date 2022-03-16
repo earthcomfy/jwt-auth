cb7c22a87bef686d846c399e8a6e793490893e95
Hannah <66206865+earthcomfy@users.noreply.github.com>
2022-03-16 10:04:58 +0300

    Add custom user model

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,9 @@ INSTALLED_APPS = [
 
     # Third-party apps
     'rest_framework',
+
+    # Local apps
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -112,3 +115,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
---
+++ users/models.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext as _
+
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username',)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
