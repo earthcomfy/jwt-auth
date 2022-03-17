c6db7519c4589651ae5e9bede41cbf62ded8b786
Hannah <66206865+earthcomfy@users.noreply.github.com>
2022-03-17 10:37:42 +0300

    Use python-decouple for secret key

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l-p66b6wx^ylc@y+(f-sms+)!1(p0gik6z+4#tx&k@m5glb&y_'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
